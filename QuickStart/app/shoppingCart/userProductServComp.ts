9a14a23811e8a99917b6b68cc55b9b50af526237
addinfinity <addinfinity@yahoo.com>
2018-01-31 10:22:33 +0530

    Day 2 Final

--- QuickStart/app/shoppingCart/userProductServComp.ts
+++ QuickStart/app/shoppingCart/userProductServComp.ts
@@ -17,12 +17,12 @@ export class UseProductServComponent{
     }
 
     getProduct():void{
-        this.productName = this.servObj.getRandomProduct();
+       // this.productName = this.servObj.getRandomProduct();
         console.log(this.productName);
     }
 
     addProduct():void{
-        this.servObj.insert(this.productToAdd);
+        //this.servObj.insert(this.productToAdd);
         console.log(            
         this.servObj.getProducts()
         );

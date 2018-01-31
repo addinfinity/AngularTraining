943089b3acc329afe377c8109f828b43252e2553
addinfinity <addinfinity@yahoo.com>
2018-01-31 17:10:57 +0530

    some sample tests

--- UnitTesting/Tests/FirstTest.js
+++ UnitTesting/Tests/FirstTest.js
@@ -1,10 +1,3 @@
-describe("A suite", function () {
-    it("contains spec with an expectation", function () {
-        expect(true).toBe(true);
-    });
-});
-
-
 //Jasmine : https://jasmine.github.io/2.9/introduction
 
 //npm install jasmine - core karma karma - jasmine
@@ -13,3 +6,21 @@ describe("A suite", function () {
 
 // npm global packages gets installed in %appdata%/npm/node_modules
 // karma init -> command to initialize the karma.conf.js configuration file
+// test bed is mocking library
+// https://angular.io/guide/testing
+
+describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        expect(true).toBe(true);
+    });
+});
+
+function add(x,y) {
+    return x+y;
+}
+
+describe("A math suite", function () {
+    it("add two numbers", function () {
+        expect(add(20,30)).toBe(50);
+    });
+});
